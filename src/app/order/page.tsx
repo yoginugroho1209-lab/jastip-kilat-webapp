75088d0090a91d42feb2260c9938101521ec463b
yoginugroho1209-lab <256835229+yoginugroho1209-lab@users.noreply.github.com>
2026-08-29 03:44:47 +0700

    fix: import useRef in order page

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Map, { Marker } from 'react-map-gl/mapbox';
